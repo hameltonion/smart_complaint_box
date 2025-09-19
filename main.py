a726dfd401f8d08510a642e9498d2f14d2ebaa71
hameltonion <toprankers9876@gmail.com>
2025-09-19 16:35:31 +0530

    added missing funcation call in main.py to check trained model files

--- main.py
+++ main.py
@@ -67,5 +67,6 @@ app.register_blueprint(admin_bp)
 # -----------------------
 if __name__ == "__main__":
     create_database()
+    train_models_if_missing()
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
